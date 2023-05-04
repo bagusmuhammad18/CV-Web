45144de98b64d5dcc1537817baa570d780d25354
bagusmuhammad18 <bagusmuhammadfajar27@gmail.com>
2023-05-04 23:14:45 +0700

    adding animation on skill container

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,23 +1,4 @@
-
-// When the user scrolls the page, execute myFunction
-// window.onscroll = function() {myFunction()};
-document.querySelector(".wrapper").onscroll = function() {myFunction()};
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  alert("masuk function");
-
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-    alert("masuksticky");
-  } else {
-    navbar.classList.remove("sticky");
-    alert("ga masuk sticky");
-  }
-}
+// disable transition on page load
+$(window).load(function() {
+  $("body").removeClass("preload");
+});
