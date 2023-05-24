a3504675cb1b5787d1f0056660b40d7d0a0ad2a1
bagusmuhammad18 <bagusmuhammadfajar27@gmail.com>
2023-05-24 23:48:17 +0700

    adding some skills and projects

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,41 +1,39 @@
+const body = document.getElementsByClassName("wrapper")[0];
+body.addEventListener("click", function (e) {
+  console.log(e.target);
+});
+
 //start showing navbar when scrolling
-const wrapper=document.querySelector(".wrapper")
-const nav=document.getElementById("navbar")
-wrapper.onscroll = function() {navShowScroll()};
+const wrapper = document.querySelector(".wrapper");
+const nav = document.getElementById("navbar");
+// wrapper.onscroll = function() {navShowScroll()};
+wrapper.addEventListener("scroll", navShowScroll);
+
 function navShowScroll() {
-  let scrolltop_=wrapper.scrollTop;
+  let scrolltop_ = wrapper.scrollTop;
+  // console.log(scrolltop_);
   if (scrolltop_ > 150) {
-    console.log(scrolltop_);
-    nav.classList.add("navScroll")
+    nav.classList.add("navScroll");
   } else {
-    nav.classList.remove("navScroll")
+    nav.classList.remove("navScroll");
   }
 }
-// disable transition on page load
-// $(window).load(function() {
-//     $("body").removeClass("preload");
-// });
 
-document.onreadystatechange = function() {noTransitionOnLoad()};
+document.onreadystatechange = function () {
+  noTransitionOnLoad();
+};
 const body_ = document.getElementsByTagName("body")[0];
-function noTransitionOnLoad(){
+function noTransitionOnLoad() {
   body_.classList.remove("preload");
 }
 
 // GoTop button on scroll
-const goTopButton=document.getElementById("goTop");
-wrapper.onscroll = function() {goTopFunction()};
-function goTopFunction(){
-  let scrolltop_=wrapper.scrollTop;
+const goTopButton = document.getElementById("goTop");
+wrapper.addEventListener("scroll", function () {
+  let scrolltop_ = wrapper.scrollTop;
   if (scrolltop_ > 600) {
-    // goTopButton.style.transition='all 0.9s';
-    // goTopButton.style.visibility='visible';
-    goTopButton.classList.add("scroll")
-
-
+    goTopButton.classList.add("scroll");
   } else {
-    // goTopButton.style.visibility='hidden';
-    goTopButton.classList.remove("scroll")
-
+    goTopButton.classList.remove("scroll");
   }
-};
+});
